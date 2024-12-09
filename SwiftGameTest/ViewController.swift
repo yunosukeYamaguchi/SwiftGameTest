d4f765d54a1d49db7196937842a7d12ff0ea4f8f
student <student@TSA-GAME04.local>
2024-12-09 18:52:33 +0900

    Initial commit

---
+++ SwiftGameTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SwiftGameTest
+//
+//  Created by student on 2024/12/09.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
